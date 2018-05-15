7de9eb569906fb86d48549907bb8177d2a7431ff
Adam LaCasse <adamlacasse@outlook.com>
2018-05-15 16:27:36 -0400

    can't get the first API (twitter) working

--- liri.js
+++ liri.js
@@ -1,20 +1,19 @@
-require("dotenv").config();
-
 var keys = require('./keys.js');
 
+// var Spotify = require('node-spotify-api');
+// var request = require('request');
+// var fs = require('fs');
+
+var Twitter = require('twitter'); 
 var client = new Twitter(keys.twitter);
-var spotify = new Spotify(keys.spotify);
-
-var inquirer = require("inquirer");
-
-inquirer.prompt([
-  {
-    type: "list",
-    name: "commands",
-    message: "Choose one of these commands, and add the search term after (e.g. a movie or song title)",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
-  }
-]).then(function(wha) {
-    console.log("inquirer loaded");
+ 
+var params = {screen_name: 'AwlUnh'};
+client.get('statuses/show/', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  } else {console.log("apparently there was an error")}
 });
+
+
+
 
