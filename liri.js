4578e36fad8e6552850c585c02a510bc12bf8971
Adam LaCasse <adamlacasse@outlook.com>
2018-05-15 14:55:16 -0400

    more updates

--- liri.js
+++ liri.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 var keys = require('./keys.js');
 
-var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 var inquirer = require("inquirer");
 
@@ -11,10 +11,10 @@ inquirer.prompt([
   {
     type: "list",
     name: "commands",
-    message: "Choose on of these commands.",
+    message: "Choose one of these commands, and add the search term after (e.g. a movie or song title)",
     choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
   }
-]).then(function(user) {
+]).then(function(wha) {
     console.log("inquirer loaded");
 });
 
