0fbdd27de50a9ecfed6c788db43296f1e5b29fff
Adam LaCasse <adamlacasse@outlook.com>
2018-05-15 10:48:19 -0400

    initial progress

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
 require("dotenv").config();
+
+var keys = require('./keys.js');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var inquirer = require("inquirer");
+
+inquirer.prompt([
+  {
+    type: "list",
+    name: "commands",
+    message: "Choose on of these commands.",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+  }
+]).then(function(user) {
+
+});
+
