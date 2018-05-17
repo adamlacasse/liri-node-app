e64ff4c63f72c5c9155647ca5ae4eeae037c2945
Adam LaCasse <adamlacasse@outlook.com>
2018-05-17 11:05:10 -0400

    spotify is working

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var keys = require('./keys.js');
 
+
 function getTweets(){
   var Twitter = require('twitter'); 
   var client = new Twitter(keys.twitter);
@@ -14,28 +15,53 @@ function getTweets(){
       }
     } else {console.log(`the error is: ${error}`)}
   });
+} // end of getTweets
+
+var getArtistNames = function(artist) {
+  return artist.name;
 }
 
+function getSpotify(songName){
+  var Spotify = require('node-spotify-api');
+  var spotify = new Spotify(keys.spotify);
+  
+  spotify.search({ 
+    type: 'track', query: songName }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    var songs = data.tracks.items; 
+    for (let i = 0; i < songs.length; i++) {
+        console.log (i);
+        console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+        console.log('song name: ' + songs[i].name);
+        console.log('preview song: ' + songs[i].preview_url);
+        console.log('album: ' + songs[i].album.name);
+        console.log('-------------------------------------------');
+    }
+  });
+} // end of getSpotify
 
-// var Spotify = require('node-spotify-api');
-// var spotifyKeys = new Spotify(keys.spotify);
 
 var userPicks = function (value1, value2) {
   switch (value1) {
-      case 'my-tweets':
-          getTweets();
-          break;
-      case 'spotify-this-song': 
-          getSpotify(value2);  
-      case 'movie-this' :
-          getMovie(value2); 
-      case 'do-what-it-says':
-          doWhatItSays(); 
-          break;
-      default:
+    case 'my-tweets':
+      getTweets();
+      break;
+    case 'spotify-this-song': 
+      getSpotify(value2);  
+      break;
+    case 'movie-this' :
+      getMovie(value2);
+      break; 
+    case 'do-what-it-says':
+      doWhatItSays(); 
+      break;
+    default:
       console.log('liri does not know that');  
   }
-}
+} // end of userPicks
 
 userPicks(process.argv[2], process.argv[3]);
 
